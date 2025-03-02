5ddc17f9107a3643a724041e8244699fdcb43cae
mattia <mattia2001@live.it>
2025-03-02 19:13:54 +0100

    feat: add routes configuration

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,11 @@
 <?php
-// filepath: /Users/mattiabarbieri/Documents/backend-up2you/routes/api.php
 
 use App\Http\Controllers\AttendeeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
-use Illuminate\Http\Request;
+use App\Http\Middleware\ApiKeyMiddleware;
 
-Route::middleware(function (Request $request, $next) {
-    $apiKey = $request->header('API_KEY');
-
-    if ($apiKey !== env('API_KEY')) {
-        return response()->json(['message' => 'Unauthorized'], 401);
-    }
-
-    return $next($request);
-})->group(function () {
+Route::middleware([ApiKeyMiddleware::class])->group(function () {
     Route::prefix('events')->group(function () {
         Route::get('/', [EventController::class, 'index']);
         Route::post('/', [EventController::class, 'store']);
